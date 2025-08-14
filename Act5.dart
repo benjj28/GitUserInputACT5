315ae413840b10d01ba3eae902f6cfb6f0d9c316
benj <168496203+benjj28@users.noreply.github.com>
2025-08-14 13:35:38 +0800

    GitUserInputACT5

---
+++ Act5.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+    print ("Enter your AGE: ");
+    int? Age = int.parse(stdin.readLineSync()!);
+    print("Your age is ${Age}");
+
+    if (Age <=18) {
+      print ("Minor");
+    }
+
+    else {
+      print ("Adult");
+    }
+}
